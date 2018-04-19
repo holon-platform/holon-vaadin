04fc74459dce5d799643b0bc3085abd0dbb3698a
Riccardo Righi <rr9379@gmail.com>
2018-04-19 12:30:36 +0200

    5.1.0 documentation updates

---
+++ documentation/src/main/java/com/holonplatform/vaadin/examples/ExampleComponents.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016-2017 Axioma srl.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.vaadin.examples;
+
+import com.holonplatform.vaadin.components.Components;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.Sizeable.Unit;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SuppressWarnings("unused")
+public class ExampleComponents {
+
+	class MyData {
+	}
+
+	public void builder1() {
+		// tag::builder1[]
+		Label lbl = Components.label() // <1>
+				.fullWidth() // <2>
+				.height(50, Unit.PIXELS) // <3>
+				.styleName("my-style").styleName(ValoTheme.LABEL_BOLD) // <4>
+				.icon(VaadinIcons.CHECK) // <5>
+				.caption("The caption") // <6>
+				.captionAsHtml() // <7>
+				.description("The description") // <8>
+				.withData(new MyData()) // <9>
+				.hidden() // <10>
+				.disabled() // <11>
+				.responsive() // <12>
+				.withAttachListener(event -> { // <13>
+					// ...
+				}).withDetachListener(event -> { // <14>
+					// ...
+				}).withContextClickListener(event -> { // <15>
+					event.isDoubleClick();
+					// ...
+				}).errorHandler(event -> { // <16>
+
+				})
+				// Label specific configuration
+				.content("Label content") // <17>
+				.html() // <18>
+				.build();
+		// end::builder1[]
+	}
+
+	private static final Component COMPONENT_1 = new Label();
+	private static final Component COMPONENT_2 = new Label();
+	private static final Component COMPONENT_3 = new Label();
+	private static final Component COMPONENT_4 = new Label();
+	private static final Component COMPONENT_5 = new Label();
+	private static final Component COMPONENT_6 = new Label();
+	private static final Component COMPONENT_7 = new Label();
+
+	public void builder2() {
+		// tag::builder2[]
+		VerticalLayout verticalLayout = Components.vl() // <1>
+				.margin() // <2>
+				.spacing() // <3>
+				.add(COMPONENT_1).add(COMPONENT_2, COMPONENT_3) // <4>
+				.addAndAlign(COMPONENT_4, Alignment.TOP_CENTER) // <5>
+				.addAndExpand(COMPONENT_5, 0.5f) // <6>
+				.addAndExpandFull(COMPONENT_6) // <7>
+				.addAlignAndExpand(COMPONENT_7, Alignment.MIDDLE_CENTER, 1f) // <8>
+				.deferLocalization().build();
+		// end::builder2[]
+	}
+
+	public void builder3() {
+		// tag::builder3[]
+		Label label = new Label();
+
+		Components.configure(label).fullWidth().content("my content").contentMode(ContentMode.PREFORMATTED); // <1>
+
+		VerticalLayout verticalLayout = new VerticalLayout();
+
+		Components.configure(verticalLayout).spacing().addAndAlign(COMPONENT_1, Alignment.TOP_CENTER) // <2>
+				.addAndExpandFull(COMPONENT_2);
+		// end::builder3[]
+	}
+
+	public void builder4() {
+		// tag::builder4[]
+		Label lbl = Components.label() //
+				.caption("Default caption", "caption.message.code") //
+				.deferLocalization() // 1>
+				.build();
+		// end::builder4[]
+	}
+
+}
--- documentation/src/main/java/com/holonplatform/vaadin/examples/ExampleInput.java
+++ documentation/src/main/java/com/holonplatform/vaadin/examples/ExampleInput.java
@@ -16,6 +16,8 @@
 package com.holonplatform.vaadin.examples;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Locale;
 
 import com.holonplatform.core.Validator;
@@ -25,41 +27,116 @@ import com.holonplatform.core.property.PropertyValueConverter;
 import com.holonplatform.vaadin.components.Components;
 import com.holonplatform.vaadin.components.Field;
 import com.holonplatform.vaadin.components.Input;
+import com.holonplatform.vaadin.components.MultiSelect;
+import com.holonplatform.vaadin.components.SingleSelect;
 import com.holonplatform.vaadin.components.ValidatableInput;
+import com.holonplatform.vaadin.components.ValueHolder;
+import com.holonplatform.vaadin.components.builders.DateInputBuilder.Resolution;
 import com.vaadin.data.Converter;
+import com.vaadin.data.HasValue;
 import com.vaadin.data.Result;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("unused")
 public class ExampleInput {
 
-	public void input1() {
-		// tag::input1[]
-		Input<String> stringInput = Components.input.string().caption("String input").inputPrompt("Value here")
-				.maxLength(100).styleName("mystyle").build(); // <1>
+	enum MyEnum {
 
-		stringInput.setValue("test"); // <2>
-		String value = stringInput.getValue(); // <3>
-
-		stringInput.addValueChangeListener(e -> System.out.println("Value: " + e.getValue())); // <4>
-		// end::input1[]
 	}
 
-	public void input2() {
-		// tag::input2[]
-		Input<LocalDate> dateInput = Components.input.localDate().caption("Date input").lenient(true).build(); // <1>
+	public void component() {
+		// tag::component[]
+		Input<String> stringInput = Components.input.string().build(); // <1>
 
-		Component inputComponent = dateInput.getComponent(); // <2>
-		// end::input2[]
+		Component component = stringInput.getComponent(); // <2>
+		VerticalLayout vl = Components.vl().add(component).build(); // <3>
+		// end::component[]
 	}
 
-	public void input3() {
-		// tag::input3[]
-		Input<String> stringInput = Input.from(new TextField());
-		// end::input3[]
+	public void builder1() {
+		// tag::builder1[]
+		Input<String> stringInput = Components.input.string().build(); // <1>
+		stringInput = Components.input.string(true).build(); // <2>
+		Input<Integer> integerInput = Components.input.number(Integer.class).build(); // <3>
+		Input<Boolean> booleanInput = Components.input.boolean_().build(); // <4>
+		Input<Date> dateInput = Components.input.date(Resolution.DAY).build(); // <5>
+		Input<LocalDate> localDateInput = Components.input.localDate().build(); // <6>
+		Input<LocalDateTime> localDateTimeInput = Components.input.localDateTime().build(); // <7>
+
+		SingleSelect<MyEnum> enumSingleSelect = Components.input.enumSingle(MyEnum.class).build(); // <8>
+		MultiSelect<MyEnum> enumMultiSelect = Components.input.enumMulti(MyEnum.class).build(); // <9>
+		// end::builder1[]
+	}
+
+	public void builder2() {
+		// tag::builder2[]
+		Input<String> stringInput = Components.input.string() //
+				// Component configuration
+				.fullWidth().styleName("my-style").caption("The caption") // <1>
+				// Input configuration
+				.readOnly() // <2>
+				.tabIndex(10) // <3>
+				.withValue("Initial value") // <4>
+				.withValueChangeListener(event -> { // <5>
+					event.getValue();
+					event.getOldValue();
+					// ...
+				}).withFocusListener(event -> { // <6>
+					// focused
+				})
+				// Specific String Input configuration
+				.inputPrompt("The prompt") // <7>
+				.maxLength(100) // <8>
+				.blankValuesAsNull(true) // <9>
+				.textChangeEventMode(ValueChangeMode.BLUR) // <10>
+				.build();
+		// end::builder2[]
+	}
+
+	public void builder3() {
+		// tag::builder3[]
+		TextField field = new TextField();
+
+		Input<String> stringInput = Input.from(field); // <1>
+		// end::builder3[]
+	}
+
+	public void builder4() {
+		// tag::builder4[]
+		TextField field = new TextField();
+
+		Input<Integer> integerInput = Input.from(field, new StringToIntegerConverter("conversion error")); // <1>
+		// end::builder4[]
+	}
+
+	public void builder5() {
+		// tag::builder5[]
+		Field<LocalDate> field = Components.input.localDate() // <1>
+				// configuration omitted
+				.asField(); // <2>
+
+		HasValue<LocalDate> hasValue = field; // <3>
+		Component component = field;
+		// end::builder5[]
+	}
+
+	public void valueChange() {
+		// tag::valuechange[]
+		Input<String> stringInput = Components.input.string() //
+				.withValueChangeListener(event -> { // <1>
+					String currentValue = event.getValue(); // <2>
+					String previousValue = event.getOldValue(); // <3>
+					boolean byUser = event.isUserOriginated(); // <4>
+					ValueHolder<String> source = event.getSource(); // <5>
+					// ...
+				}).build();
+		// end::valuechange[]
 	}
 
 	public void input4() {
@@ -106,17 +183,16 @@ public class ExampleInput {
 		Input<Integer> integerInput = Components.input.number(Integer.class).build();
 
 		Input<Boolean> booleanInput = Input.from(integerInput, // <1>
-				Converter.from(value -> Result.ok((value == null) ? null : (value ? 1 : 0)),
-						value -> (value == null) ? Boolean.FALSE : (value.intValue() > 0)));
+				Converter.from(value -> Result.ok((value == null) ? Boolean.FALSE : (value.intValue() > 0)),
+						value -> (value == null) ? null : (value ? 1 : 0)));
 
 		Boolean boolValue = booleanInput.getValue();
 
 		final Property<Boolean> BOOL_PROPERTY = PathProperty.create("bool", Boolean.class);
 		booleanInput = Input.from(integerInput, BOOL_PROPERTY, PropertyValueConverter.numericBoolean(Integer.class)); // <2>
 
-		Input<Long> longInput = Input.from(new TextField(), // <3>
-				Converter.from(value -> Result.ok((value == null) ? null : value.toString()),
-						value -> (value == null || value.trim().isEmpty()) ? null : Long.parseLong(value)));
+		Input<Integer> longInput = Input.from(new TextField(), // <3>
+				new StringToIntegerConverter("Conversion error"));
 		// end::input7[]
 	}
 
